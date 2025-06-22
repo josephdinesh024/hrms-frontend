c2d0cb84124696ead5c612a984bbf997f05f5c94
josephdinesh024 <josephdinesh24@gmail.com>
2025-06-22 07:09:03 +0530

    new version 20/6

--- static/js/confic.js
+++ static/js/confic.js
@@ -1,5 +1,6 @@
 
-const DomainURL = "https://hrms-api-s8dh.onrender.com" //"http://localhost:8080"
+const DomainURL = "https://hrms-api-s8dh.onrender.com" 
+// const DomainURL = "http://localhost:8080"
 
 // Permission Modules
 const modules = ["user","access","timesheet","timeoffpolicy","appraisal","appraisalform","role","team","request","permission","management","onboard"]
